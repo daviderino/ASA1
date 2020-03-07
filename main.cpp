472d3758b0359f9b54b1ab0f6ec7f26bd2b63f2a
Goncas2 <goncalo.fernandes2@gmail.com>
2020-03-07 21:51:35 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 
 int main() {
-	const std::string delimiter = ",";
 	std::string input, input_aux;
-	size_t comma = 0;
 
-	getline(std::cin, input);
-
-	std::cout << input << std::endl;
+	getline(std::cin, input, ',');
+	getline(std::cin, input_aux);
 	
-	comma = input.find(delimiter);
-	input_aux = input.substr(0, comma);
-	input.erase(0, comma + delimiter.length());
-
 	const int N = std::stoi(input_aux);
 	const int M = std::stoi(input);
 
