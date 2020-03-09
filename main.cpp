697b35def9abda4fe91eca639f958fcde0fccdfe
daviderino <david.baptista00@hotmail.com>
2020-03-09 11:50:40 +0000

    :)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
+
+struct vertix {
+	int v;
+	struct vertix* next;
+};
+
+class Graph {
+private: 
+	int V;
+	int E;
+	std::vector<vertix> adj;
+public:
+	Graph(int V, int E) {
+		this->V = V;
+		this->E = E;
+	}
+
+	void addVertix(int v) {
+		struct vertix node;
+		node.v = v;
+		node.next = nullptr;
+		adj.push_back(node);
+	}
+
+};
+
 
 int main() {
 	std::string input1, input2;
+	std::string inputVertix;
 
 	getline(std::cin, input1, ',');
 	getline(std::cin, input2);
@@ -9,7 +37,18 @@ int main() {
 	const int N = std::stoi(input1);
 	const int M = std::stoi(input2);
 
-	std::cout << "N: " << N << "\nM: " << M << std::endl;
+	Graph *graph = new Graph(N, M);
+
+
+	for(int i = 0; i < N; i++) {
+		getline(std::cin, inputVertix);
+		const int value = std::stoi(inputVertix);
+		graph->addVertix(value);
+	}
+
+	for(int i = 0; i < M; i++) {
+
+	}
 
     return 0;
 }
